54e4cbbdb92aa58e5627596fab198c6cf196f589
Eugene Yamenko <yamenko@me.com>
2015-04-19 00:15:43 +1000

    MVC will be replaced with SPA application

---
+++ BRMS.MVC/Controllers/ServicePackagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BRMS.MVC.Controllers
+{
+    public class ServicePackagesController : Controller
+    {
+        // GET: ServicePackages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
